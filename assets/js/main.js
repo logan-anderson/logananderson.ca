9182e4fe3e52ed0e0d80922585b53b70850c214b
logan-anderson <landerson2@upei.ca>
2019-07-14 21:31:29 -0300

    added scolling animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,29 +1,55 @@
+let the_body = document.querySelector('body')
 
 
-
-anime({
-    targets: 'div.box.red',
-    translateY: 250,
-    direction: 'alternate',
-    duration: 1000,
-    loop: true,
+let scroll_test = anime({
+  targets: 'div.box.red',
+  translateY: 250,
+  direction: 'alternate',
+  duration: 1000,
+  autoplay: false
 });
-var lineDrawing = anime({
-    targets: '#lineDrawing .lines path',
-    strokeDashoffset: [anime.setDashoffset, 0],
-    easing: 'easeInOutSine',
-    duration: 3000,
-    delay: function(el, i) { return i * 250 },
-    direction: 'alternate',
-    loop: true
-  });
+setInterval(() => {
+  scroll_test.seek(window.scrollY);
+}, 50)
 
-  var logan_anime = anime({
-    targets: '#test .test-lines .test-path',
-    strokeDashoffset: [anime.setDashoffset, 0],
-    easing: 'easeInOutSine',
-    duration: 3000,
-    delay: function(el, i) { return i * 250 },
-    direction: 'alternate',
-    loop: true
-  });
+
+let scroll_top = anime({
+  targets: '#logan-svg',
+  duration: 2000,
+  translateY: 150,
+  autoplay: false,
+  scale: {
+    value: .01,
+    duration: 2500,
+  },
+
+});
+setInterval(() => {
+  scroll_top.seek(window.scrollY);
+}, 50)
+
+
+
+var lineDrawing = anime({
+  targets: '#lineDrawing .lines path',
+  strokeDashoffset: [anime.setDashoffset, 0],
+  easing: 'easeInOutSine',
+  duration: 3000,
+  delay: function (el, i) {
+    return i * 250
+  },
+  direction: 'alternate',
+  loop: true
+});
+
+var logan_anime = anime({
+  targets: '#test .test-lines .test-path',
+  strokeDashoffset: [anime.setDashoffset, 0],
+  easing: 'easeInOutSine',
+  duration: 3000,
+  delay: function (el, i) {
+    return i * 250
+  },
+  direction: 'alternate',
+  loop: true
+});
